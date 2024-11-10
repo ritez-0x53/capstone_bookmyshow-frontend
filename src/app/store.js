aff632e4de4e94f306399e0e24467ea8d3a646bb
ritez_0x53 <110099293+ritez-0x53@users.noreply.github.com>
2024-11-10 15:41:48 +0530

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,15 @@
+// Import the showSlice reducer from the shows feature slice
 import showSlice from "../features/shows/showSlice";
+// Import the configureStore function from Redux Toolkit to create the store
 import { configureStore } from "@reduxjs/toolkit";
 
+// Configure the Redux store, combining the showSlice reducer
 const store = configureStore({
   reducer: {
+    // The 'shows' key will hold the state managed by the showSlice reducer
     shows: showSlice,
   },
 });
 
+// Export the store to be used in the app's root component
 export { store };
