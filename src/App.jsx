dedd0e56955ba6fef57b1dcadbfe795953e0b242
ritez_0x53 <110099293+ritez-0x53@users.noreply.github.com>
2024-11-10 15:44:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,33 @@ import LastOrder from './components/LastOrder'
 import Spinner from './components/Spinner'
 
 function App() {
+  // State to manage the loading process
   const [processing, setProcessing] = useState(true);
 
+  // useEffect to simulate a loading delay of 2 seconds
   useEffect(() => {
     setTimeout(() => {
-      setProcessing(false)
+      setProcessing(false) // Update processing state after timeout
     }, 2000)
   }, [])
 
-  return <div id='app' className='overflow-hidden' >
-    <h1 className='text-2xl mb-3 font-bold' >Book that show!!</h1>
-    <div className='wrapper' >
-      <BookForm setProcessing={setProcessing} />
-      <LastOrder />
-      {
-        processing ? <Spinner /> : null
-      }
+  return (
+    <div id='app' className='overflow-hidden'>
+      <h1 className='text-2xl mb-3 font-bold'>Book that show!!</h1>
+      <div className='wrapper'>
+        {/* Booking form component with setProcessing prop to control loading state */}
+        <BookForm setProcessing={setProcessing} />
+       
+        {/* Component to display the last booking order */}
+        <LastOrder />
+        
+        {
+          // Conditionally render Spinner component while processing is true
+          processing ? <Spinner /> : null
+        }
+      </div>
     </div>
-  </div>
-
+  )
 }
 
 export default App
