a7b92287e050b1dc1122a7d955469f8e3bc16329
ritez_0x53 <110099293+ritez-0x53@users.noreply.github.com>
2024-11-10 15:45:05 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
+// Import React for JSX syntax
 import React from 'react'
+// Import ReactDOM for rendering the React application to the DOM
 import ReactDOM from 'react-dom/client'
+// Import the main App component for rendering
 import App from './App'
+// Import global CSS styles for the application
 import './index.css'
+// Import Provider component from React-Redux to provide the Redux store to the app
 import { Provider } from 'react-redux'
+// Import the Redux store configuration from the app folder
 import { store } from './app/store'
 
+// Render the app into the root element in the HTML, and wrap it with the Redux Provider
+// This allows all components in the app to access the Redux store
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
